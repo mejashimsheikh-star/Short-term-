58d2fd0f3b85f78b1d6e5e658897b5bd47848be1
mejashimsheikh-star <mejashimsheikh@gmail.com>
2026-07-13 05:21:12 +0600

    Update market.py

--- backend/market.py
+++ backend/market.py
@@ -1,15 +1,37 @@
 import httpx
+
 from config import COINGECKO_API, SUPPORTED_COINS
 
 
 async def get_market_prices():
+
     params = {
         "ids": ",".join(SUPPORTED_COINS),
         "vs_currencies": "usd",
         "include_24hr_change": "true"
     }
 
-    async with httpx.AsyncClient(timeout=10) as client:
-        response = await client.get(COINGECKO_API, params=params)
-        response.raise_for_status()
-        return response.json()
+
+    try:
+
+        async with httpx.AsyncClient(
+            timeout=10
+        ) as client:
+
+            response = await client.get(
+                COINGECKO_API,
+                params=params
+            )
+
+
+            response.raise_for_status()
+
+
+            return response.json()
+
+
+    except Exception as e:
+
+        return {
+            "error": str(e)
+        }
